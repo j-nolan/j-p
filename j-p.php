fb6093499020d4ed6f857a719b4748d1549d1e8d
James Nolan <j.nolan@otherwise.ch>
2016-10-03 16:55:45 +0200

    place source files into separate directory

--- j-p.php
+++ j-p.php
@@ -9,7 +9,7 @@
  
 
 // Requires
-require_once( 'functions.php' );
+require_once( 'src/functions.php' );
 
 // Admin page
 add_action('admin_menu', 'register_jp_custom_menu_page');
@@ -18,14 +18,13 @@ function register_jp_custom_menu_page () {
     add_submenu_page( 'journee-pedagogique', 'Accueil', 'Accueil', 'manage_options', 'journee-pedagogique');
     add_submenu_page( 'journee-pedagogique', 'Gestion des journées pédagogiques', 'Gestions de journées pédagogiques', 'manage_options', 'journee-pedagogique-create', 'create_new_jp');
     add_submenu_page( 'journee-pedagogique', 'Participants', 'Participants', 'manage_options', 'journee-pedagogique-participants', 'display_jp_participants');
-    //add_submenu_page( 'journee-pedagogique', 'Participants (brute)', 'Participants (brute)', 'manage_options', 'journee-pedagogique-participants-brute', 'download_jp_participants');
     add_submenu_page( 'journee-pedagogique', 'Statistiques', 'Statistiques', 'manage_options', 'journee-pedagogique-statistiques', 'display_jp_statistiques');
     add_submenu_page( 'journee-pedagogique', 'Répartition automatique', 'Répartition automatique', 'manage_options', 'journee-pedagogique-repartition', 'display_jp_repartition');
 }
 
 // CSS
 function jp_admin_theme_style() {
-    wp_enqueue_style('journee-pedagogique-style', plugins_url('wp-admin.css', __FILE__));
+    wp_enqueue_style('journee-pedagogique-style', plugins_url('src/wp-admin.css', __FILE__));
 }
 add_action('admin_enqueue_scripts', 'jp_admin_theme_style');
 add_action('login_enqueue_scripts', 'jp_admin_theme_style');
